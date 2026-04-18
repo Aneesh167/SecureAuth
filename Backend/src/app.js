29f8851fa0e40f56b18c376bfe28af36903274f4
Aneesh167 <aneeshmehta2005@gmail.com>
2026-04-18 14:02:20 +0530

    updating cors and cookies

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,16 +4,20 @@ import authRoute from "../src/routes/auth.routes.js";
 import cors from "cors";
 import { config } from "./config/config.js";
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(
   cors({
     origin: config.CLIENT_USER,
     credentials: true,
   }),
 );
+
+// ✅ Handle preflight requests explicitly
+app.options("*", cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use("/api/auth", authRoute);
 
 export default app;
