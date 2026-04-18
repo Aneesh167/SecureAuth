1d977fd59cbb0c6808dfeff19a5005342e82379e
Aneesh167 <aneeshmehta2005@gmail.com>
2026-04-18 14:12:08 +0530

    updating cors and cookies

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,9 +11,6 @@ app.use(
   }),
 );
 
-// ✅ Handle preflight requests explicitly
-app.options("*", cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
